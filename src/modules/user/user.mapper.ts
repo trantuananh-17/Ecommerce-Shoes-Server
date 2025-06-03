b62b5a69fcfe8594d1f1b88ac45d82b2c0daaac3
TunAnhhaui <anhkyohauik17@gmail.com>
2025-06-03 17:03:46 +0700

    feat create base address

--- src/modules/user/user.mapper.ts
+++ src/modules/user/user.mapper.ts
@@ -1,5 +1,15 @@
 import { IUser } from "./models/user.model";
-import { IUserInfoResponseDto } from "./user.dto";
+import { IAuthInfoResponseDto, IUserInfoResponseDto } from "./user.dto";
+
+export const authInfoResponseMapper = (user: IUser): IAuthInfoResponseDto => {
+  return {
+    id: user._id.toString(),
+    email: user.email,
+    fullname: user.fullname,
+    role: user.role,
+    loginType: user.loginType || "",
+  };
+};
 
 export const userInfoResponseMapper = (user: IUser): IUserInfoResponseDto => {
   return {
